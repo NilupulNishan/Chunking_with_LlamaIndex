ae24b687bbebcee11dece1aa082737fdc93d4bc1
Nilupul Nishan <nilupulofficial2000@gmail.com>
2026-01-23 13:53:43 +0530

    with new approch to save metadata. created pdf_loader.py, update settings, src-__init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,13 @@
 """
 PDF Embeddings System - Source Package
 """
-from .pdf_processor import PDFProcessor
+from .pdf_loader import PDFLoader
 from .embeddings import EmbeddingsManager
 from .chunker import DocumentChunker
 from .query_engine import QueryEngine, MultiCollectionQueryEngine
 
 __all__ = [
-    'PDFProcessor',
+    'PDFLoader',
     'EmbeddingsManager',
     'DocumentChunker',
     'QueryEngine',
